c3e794a81fdc46c08b4c00307b8a989b006a44e0
NicholasWhit3 <64132625+NicholasWhit3@users.noreply.github.com>
2020-12-01 23:14:31 +0200

    Update driver.h

--- driver.h
+++ driver.h
@@ -9,7 +9,7 @@ public:
     Driver();
     Driver(std::string dbName);
    virtual ~Driver();
-   virtual bool create_table(std::string tableName,const std::vector <std::string> &header) = 0;                      // "= 0" чисто виртуальный метотод. Без объекта лишь с указателем на объект.
+   virtual bool create_table(std::string tableName,const std::vector <std::string> &header) = 0;              
    virtual bool insert_raw(std::string tableName,const  std::vector <std::string> &data) = 0;
    virtual bool load_table(std::string tableName, std::vector <std::string> &data) = 0;
     protected:
